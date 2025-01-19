439097d7d79b43807b64259e01f1bd9d745dde03
dluman <dluman@allegheny.edu>
2025-01-18 21:41:29 -0500

    Updating with dependencies

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// TODO: Write all of badger
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const async = require('async');
+const yaml = require('js-yaml');
+
+const octokit = github.getOctokit(
+  process.env.GITHUB_TOKEN
+);
+
+const run = () => {
+
+}
+
+run();
